35dbee4a19320d76ba4064bddc19f7540acfdd79
dzul <rhezaa_algadhari@yahoo.co.id>
2017-12-03 22:22:07 +0700

    fix listview

--- app/src/main/java/com/example/pc1/perpuskami/ListBook.java
+++ app/src/main/java/com/example/pc1/perpuskami/ListBook.java
@@ -36,10 +36,10 @@ public class ListBook extends AppCompatActivity {
 
         Collections.sort(judulBuku);
 
-       ArrayAdapter<String> adapterr = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, judulBuku);
-
+      // ArrayAdapter<String> adapterr = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, judulBuku);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, judulBuku);
        final ListView listView = (ListView) findViewById(android.R.id.list);
-       listView.setAdapter(adapterr);
+       listView.setAdapter(adapter);
 
         Collections.sort(daftarbukuList, new Comparator<DaftarBuku>() {
             @Override
